d66b6caf8986edca7fdd34d2abc70702e13ce12b
Nishchit Bhandari <nbhellossss@gmail.com>
2024-09-28 15:58:57 +0545

    Add: removed files from gitignore

---
+++ names.py
@@ -0,0 +1,15 @@
+names = [
+    "Liam", "Emma", "Noah", "Olivia", "James", "Ava", "Logan", "Isabella", "Benjamin", "Sophia",
+    "Mason", "Mia", "Elijah", "Amelia", "Oliver", "Harper", "Jacob", "Evelyn", "Lucas", "Abigail",
+    "Michael", "Emily", "Alexander", "Ella", "Ethan", "Elizabeth", "Daniel", "Sofia", "Matthew", "Avery",
+    "Aiden", "Scarlett", "Henry", "Grace", "Joseph", "Chloe", "Samuel", "Victoria", "David", "Riley",
+    "Jackson", "Aria", "Sebastian", "Lily", "Carter", "Aubrey", "Owen", "Zoey", "Wyatt", "Penelope",
+    "John", "Lillian", "Jack", "Addison", "Luke", "Eleanor", "Jayden", "Hannah", "Dylan", "Layla",
+    "Grayson", "Natalie", "Levi", "Brooklyn", "Isaac", "Zoe", "Gabriel", "Nora", "Julian", "Savannah",
+    "Mateo", "Audrey", "Anthony", "Skylar", "Jaxon", "Claire", "Lincoln", "Lucy", "Joshua", "Paisley",
+    "Christopher", "Everly", "Andrew", "Anna", "Theodore", "Caroline", "Caleb", "Nova", "Ryan", "Genesis",
+    "Asher", "Emilia", "Nathan", "Kennedy", "Thomas", "Samantha", "Leo", "Maya", "Isaiah", "Willow",
+    "Charles", "Kinsley", "Josiah", "Naomi", "Hudson", "Aaliyah", "Christian", "Elena", "Hunter", "Sarah",
+    "Connor", "Ariana", "Eli", "Allison", "Ezra", "Gabriella", "Aaron", "Alice", "Adrian", "Madelyn",
+    "Jonathan", "Cora", "Nolan", "Ruby", "Jeremiah", "Eva", "Easton", "Serenity"
+]
